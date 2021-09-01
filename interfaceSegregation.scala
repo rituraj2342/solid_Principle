dd5c38a90e024a010de0dd793c23d39cf1376a76
rituraj2342 <82821371+rituraj2342@users.noreply.github.com>
2021-09-01 16:09:28 +0530

    interface segregation

--- interfaceSegregation.scala
+++ interfaceSegregation.scala
@@ -15,8 +15,10 @@ trait myTrait2{
   def rectangle(side1:Int, side2:Int): Int = side1 * side2
 }
 class shape1 extends myTrait1 {
+  override def square(side1: Int, side2: Int): Int = side1 * side2
 }
 class shape2 extends myTrait2{
+  override def rectangle(side1:Int, side2:Int): Int = side1 * side2
 }
 object Main1{
   def main(args: Array[String]): Unit = {
